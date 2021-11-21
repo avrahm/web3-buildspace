debbaabf70d91724c721b2c37d37fdc77549be9c
Avrahm <avrahmk@me.com>
2021-11-20 22:59:36 -0500

    project 1 - compile contract locally

---
+++ eth-solidity-wave-portal/scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    waveContract.deployed();
+    console.log("Deployed WavePortal contract to: " + waveContract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+runMain();
