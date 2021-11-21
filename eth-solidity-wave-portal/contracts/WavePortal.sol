60b132fb1cb1727ee5e027f2b2848c44ef6700b6
Avrahm <avrahmk@me.com>
2021-11-21 14:05:03 -0500

    Project 1 - store data on blockchain

--- eth-solidity-wave-portal/contracts/WavePortal.sol
+++ eth-solidity-wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 public totalWaves;
+
     constructor() {
         console.log("gm Buildspace.. Wave!");
+    }
+
+    function wave() public {
+        totalWaves++;
+        console.log("%s, gm Wave!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- eth-solidity-wave-portal/scripts/run.js
+++ eth-solidity-wave-portal/scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     waveContract.deployed();
+
     console.log("Deployed WavePortal contract to: " + waveContract.address);
+    console.log("Owner:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves()
 };
 
 const runMain = async () => {
