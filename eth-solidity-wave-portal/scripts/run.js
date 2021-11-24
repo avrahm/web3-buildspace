2b7a22d48abd87b9e37255f0dbc670d665c7152e
Avrahm <avrahmk@me.com>
2021-11-23 22:35:39 -0500

    project 1 - section 1 - adding comments

--- eth-solidity-wave-portal/scripts/run.js
+++ eth-solidity-wave-portal/scripts/run.js
@@ -1,23 +1,32 @@
 const main = async () => {
+    // storing the address of the owner of the contract and a random account
     const [owner, randomPerson] = await hre.ethers.getSigners();
+
+    // create a new contract instance
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     waveContract.deployed();
 
+    // logging the address of the contract and owner 
     console.log("Deployed WavePortal contract to: " + waveContract.address);
     console.log("Owner:", owner.address);
 
+    // waiting for the total waves from the contract and setting the value to the variable waveCount
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
 
+    // calling the function wave() and storing the result
     let waveTxn = await waveContract.wave();
     await waveTxn.wait();
 
+    // logging the new value of the variable waveCount
     waveCount = await waveContract.getTotalWaves();
 
+    // caling the function wave() again and storing the result
     waveTxn = await waveContract.connect(randomPerson).wave();
     await waveTxn.wait();
 
+    // logging the new value of the variable waveCount
     waveCount = await waveContract.getTotalWaves()
 };
 
