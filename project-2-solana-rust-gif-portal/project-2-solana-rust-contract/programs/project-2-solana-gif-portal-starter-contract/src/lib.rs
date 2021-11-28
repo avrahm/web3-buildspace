8516f55b55b651523fcc58850b30157c403ba9d0
Avrahm <avrahmk@me.com>
2021-11-28 12:56:39 -0500

    project 2 - section 2 - setting up first solana program to do nothing. lib.rs, and tests/

--- project-2-solana-rust-gif-portal/project-2-solana-rust-contract/programs/project-2-solana-gif-portal-starter-contract/src/lib.rs
+++ project-2-solana-rust-gif-portal/project-2-solana-rust-contract/programs/project-2-solana-gif-portal-starter-contract/src/lib.rs
@@ -1,14 +1,24 @@
+// use is the equivalent to the import statement 
 use anchor_lang::prelude::*;
 
+// the id of the program and tells solana how to run the program
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+// #[] is a macro http://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/first-edition/macros.html
+// They allows us to define a function that takes no arguments and returns nothing
+// sorta like inheriting a class
 #[program]
+// pub mod is a rust module. https://stevedonovan.github.io/rust-gentle-intro/4-modules.html
+// Pub is the equivalent to the public keyword. A module is an easy way to define a collection of function and variables
 pub mod project_2_solana_gif_portal_starter_contract {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    // start_stuff_off takes some Context and outputs a ProgramResult
+    // OK(()) is a ProgramResult that is a success https://doc.rust-lang.org/std/result/
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
         Ok(())
     }
 }
 
+// another macro
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
--- project-2-solana-rust-gif-portal/project-2-solana-rust-contract/tests/project-2-solana-gif-portal-starter-contract.js
+++ project-2-solana-rust-gif-portal/project-2-solana-rust-contract/tests/project-2-solana-gif-portal-starter-contract.js
@@ -1,14 +1,27 @@
 const anchor = require('@project-serum/anchor');
 
-describe('project-2-solana-gif-portal-starter-contract', () => {
-
-  // Configure the client to use the local cluster.
+const main = async () => {
+  console.log("Starting test ...");
+// tell Anchor to set our provider. this data comes from solana config get
   anchor.setProvider(anchor.Provider.env());
+  
+  // automatically compile lib.rs and deploy to a validator
+  const program = anchor.workspace.Project2SolanaGifPortalStarterContract;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Project2SolanaGifPortalStarterContract;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  // we call our function and await for a validator to "mine" the instruction
+  const transaction = await program.rpc.startStuffOff();
+
+  console.log("Transaction:", transaction);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
